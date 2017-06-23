2e4fb47247e249039b610c909b2066eba6469639
Tiberiu Tesileanu <ttesileanu@gmail.com>
2017-06-23 15:39:49 -0400

    Added setup_paths script to add all the subfolders to the path. Updated README to state this (but much more work needs to be done on the README).

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Analysis of texture statistics
 ------------------------------
 
 This contains the scripts we're using, based on Hermundstad et al. (2014).
+
+Usage
+-----
+Run `setup_paths.m` to temporarily add all folders to the path.
---
+++ setup_paths.m
@@ -0,0 +1,25 @@
+function setup_paths
+% SETUP_PATHS Temporarily add texture analysis folders to Matlab's path.
+%   SETUP_PATHS adds all the folders containing texture analysis routines
+%   to Matlab's path. The change will only stay in effect until Matlab
+%   quits, so SETUP_PATHS needs to be called every time Matlab is
+%   restarted.
+
+% add current folder
+PWD = pwd;
+addpath(PWD);
+
+% add subfolders
+addpath(fullfile(PWD, 'analysis'));
+addpath(fullfile(PWD, 'batch'));
+addpath(fullfile(PWD, 'filters'));
+addpath(fullfile(PWD, 'graphing'));
+addpath(fullfile(PWD, 'gui'));
+addpath(fullfile(PWD, 'indices'));
+addpath(fullfile(PWD, 'misc'));
+addpath(fullfile(PWD, 'preprocess'));
+addpath(fullfile(PWD, 'save'));
+addpath(fullfile(PWD, 'segment'));
+addpath(fullfile(PWD, 'texture'));
+
+end
