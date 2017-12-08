dc2aa84a463b812738f39be7f08e8e541f4236db
Tiberiu Tesileanu <ttesileanu@gmail.com>
2017-12-08 13:32:49 -0500

    Added a small script to verify that indeed the continuous analysis is just averaged stochastic binary analysis. Fixed a typo in stochasticBinarize's doctstring.

--- preprocess/stochasticBinarize.m
+++ preprocess/stochasticBinarize.m
@@ -1,6 +1,6 @@
 function img = stochasticBinarize(img)
 % stochasticBinarize Binarize images using a stochastic approach.
-%   imgOut = stochasticBinarie(img) binarizes the matrix `img` by using a
+%   imgOut = stochasticBinarize(img) binarizes the matrix `img` by using a
 %   stochastic approach: a pixel will be turned white (i.e., equal to 1)
 %   with probability given by the initial value of the pixel, `img(i, j)`.
 
