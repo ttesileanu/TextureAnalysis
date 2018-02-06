ce022b8ff6cd8b218d3bb79c76cad06ec1d3fc72
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-02-06 11:15:35 -0500

    Generalized axis generator to work for multiple directions (though it needs extra work).

--- analysis/PatchAxisGenerator.m
+++ analysis/PatchAxisGenerator.m
@@ -9,6 +9,11 @@ classdef PatchAxisGenerator < handle
     %   patches at the center of the coordinate system, to the edge of the
     %   space.
     %
+    %   More specifically, the texture axis is the line connecting the
+    %   origin of texture space, [1/n, ..., 1/n] (where 'n' is the number
+    %   of gray levels), and the location identified by the 'axis'
+    %   argument.
+    %
     %   Use the 'samples' method to generate samples at the current
     %   locations, and the 'next' method to move to the next location. Note
     %   that 'next' must be called once after object creation and before
@@ -16,10 +21,12 @@ classdef PatchAxisGenerator < handle
     %
     %   The number of locations is set to a default value, but can be
     %   changed (using the 'nLocations' property) before the first call to
-    %   'next'. Alternatively, the actual locations that are used can be
-    %   set using the 'locations' property. A value of 0 corresponds to the
-    %   center of the coordinate system, [1/n, ..., 1/n] (where 'n' is the
-    %   number of gray levels), while 1 corresponds to the coordinates
+    %   'next'. The start and end locations are set to 0 and the maximum
+    %   allowed value, respectively, but can be changed using the
+    %   'minLocation' and 'maxLocation' properties. Alternatively, the
+    %   actual locations that are used can be set using the 'locations'
+    %   property. A value of 0 corresponds to the center of the coordinate
+    %   system, 1/n, ..., 1/n], while 1 corresponds to the coordinates
     %   provided in the 'axis' argument to the constructor. Intermediate
     %   values are mapped to texture coordinates using linear interpolation.
     %
@@ -31,30 +38,54 @@ classdef PatchAxisGenerator < handle
     %   The patch size can be a scalar or a 2-component vector, for
     %   generating rectangular patches.
     %
-    %   Example:
+    %   obj = PatchAxisGenerator({group1, group2, ...}, axesMatrix, patchSize)
+    %   generates patches along an axis in which several different texture
+    %   groups are set. The directions along which the samples are
+    %   generated in each texture group are given by the rows of the axis
+    %   parameter.
     %
-    %   % create generator for 3-gray-levels, 32x32 patches in the
-    %   % [0.5 0.5 0] direction of the AB_1_2 texture plane
-    %   generator = PatchAxisGenerator('AB_1_2', [0.5 0.5 0], 32);
+    %   Example single group:
     %
-    %   % set the number of locations along the axis
-    %   generator.nLocations = 16;
+    %       % create generator for 3 gray-levels, 32x32 patches in the
+    %       % [0.5 0.5 0] direction of the AB_1_2 texture plane
+    %       generator = PatchAxisGenerator('AB_1_2', [0.5 0.5 0], 32);
     %
-    %   % choose number of samples per location
-    %   nSamples = 8;
+    %       % set the number of locations along the axis
+    %       generator.nLocations = 16;
     %
-    %   % generate
-    %   while generator.next
-    %       patches = generator.samples(nSamples);
-    %   end
+    %       % choose number of samples per location
+    %       nSamples = 8;
+    %
+    %       % generate
+    %       while generator.next
+    %           patches = generator.samples(nSamples);
+    %       end
+    %
+    %   Example multiple groups:
+    %
+    %       % create generator for 2 gray-levels, 32x64 patches in the
+    %       % [0.9 0.1] direction of the AD_1_1 texture plane and the
+    %       % [0 1] direction of the ABC_1_1_1 texture plane
+    %       generator = PatchAxisGenerator({'AD_1_1', 'ABC_1_1_1'}, ...
+    %           [0.9 0.1 ; 0 1], [32 64]);
+    %
+    %       % set the number of locations along the axis
+    %       generator.nLocations = 10;
+    %
+    %       % generate one patch at a time
+    %       while generator.next
+    %           patch = generator.samples;
+    %       end
     
     properties
         nGray = [];         % number of gray levels
         patchSize = [];     % size of patch (scalar or 2-element vector)
-        group = '';         % texture group
-        axis = [];          % axis in texture space
+        group = '';         % texture group(s)
+        axis = [];          % axis/axes in texture space
         locations = [];     % locations where to sample
         nLocations = 16;    % number of locations to use
+        minLocation = 0;    % starting location
+        maxLocation = [];   % ending location
     end
     
     properties (Access=private)
@@ -69,15 +100,31 @@ classdef PatchAxisGenerator < handle
     
     methods
         function obj = PatchAxisGenerator(group0, axis0, patchSize0)
+            % handle square or rectangular patch sizes
             if isscalar(patchSize0)
                 patchSize0 = [patchSize0 patchSize0];
             end
+            % handle single or multi-group
+            if ~iscell(group0)
+                group0 = {group0};
+            end
+            % make sure we have an axis for each texture group
+            if length(group0) ~= size(axis0, 1)
+                error('PatchAxisGenerator:badngrp', ...
+                    'The number of rows of the axis argument should match the number of groups.');
+            end
             
-            axis0 = axis0(:);
-            obj.nGray = length(axis0);
+            % store the parameters
+            obj.nGray = size(axis0, 2);
             obj.patchSize = patchSize0;
             obj.group = group0;
-            obj.axis = axis0/sum(axis0);
+            
+            % make sure that each axis is a properly normalized probability
+            % distribution
+%             if any(axis0(:) < 0)
+%                 error('PatchAxisGenerator:negax', 'All axis components should be non-negative.');
+%             end
+            obj.axis = bsxfun(@rdivide, axis0, sum(axis0, 2));
         end
         
         function init(obj)
@@ -93,28 +140,37 @@ classdef PatchAxisGenerator < handle
             obj.btcMakemapsOpts_.area = obj.patchSize;
             obj.auxOpts_ = btc_auxopts; % won't need metropolis arguments here
             
-            % 3 grayscale levels
+            % set number of grayscale levels
             obj.mtcs_ = mtc_define(obj.nGray);
             
             % set up the locations, unless explicitly provided
             if ~isempty(obj.locations)
                 obj.locations_ = obj.locations;
             else
-                % 1/n*(1-t) + a*t >= 0
-                % 1/n + (a-1/n)*t >= 0
-                %   a) if a>1/n, then t >= -1/n / (a-1/n) = -1/(a*n - 1)
-                %       --> not a problem
-                %   b) if a<1/n, then t <= -1/n / (a-1/n) = 1/(1 - a*n)
-                %       --> t <= min(1/(1-a*n)) over all axes
-                %           t <= 1/max(1-a*n)
-                %           t <= 1/(1 - min(a)*n)
-                minAxisCoord = min(obj.axis);
-                if abs(minAxisCoord*obj.nGray - 1) > obj.tol_
-                    maxT = 1/(1 - minAxisCoord*obj.nGray);
-                    obj.locations_ = linspace(0, maxT, obj.nLocations);
+                if isempty(obj.maxLocation)
+                    % XXX this works for single texture group, but it
+                    %     doesn't make sense for multi-group
+                    
+                    % 1/n*(1-t) + a*t >= 0
+                    % 1/n + (a-1/n)*t >= 0
+                    %   a) if a>1/n, then t >= -1/n / (a-1/n) = -1/(a*n - 1)
+                    %       --> not a problem; can't happen for all coords
+                    %   b) if a<1/n, then t <= -1/n / (a-1/n) = 1/(1 - a*n)
+                    %       --> t <= min(1/(1-a*n)) over all axes
+                    %           t <= 1/max(1-a*n)
+                    %           t <= 1/(1 - min(a)*n)
+                    %   c) if all coordinates = 1/n, then we're just staying at
+                    %       the center
+                    minAxisCoord = min(obj.axis(:));
+                    if abs(minAxisCoord*obj.nGray - 1) > obj.tol_
+                        maxT = 1/(1 - minAxisCoord*obj.nGray);
+                    else
+                        maxT = 0;
+                    end
                 else
-                    obj.locations_ = zeros(1, obj.nLocations);
+                    maxT = obj.maxLocation;
                 end
+                obj.locations_ = linspace(0, maxT, obj.nLocations);
             end
             % start at the first location
             obj.current_ = 0;
@@ -146,17 +202,20 @@ classdef PatchAxisGenerator < handle
             end
             
             if obj.current_ < 1
-                error('PatchAxisGenerator:sample', 'The ''next'' method must be called before samples can be generated.');
+                error('PatchAxisGenerator:noinit', ...
+                    'The ''next'' method must be called once before samples can be generated.');
             end
             
             res = zeros([obj.patchSize(:)' n]);
             crtLocation = obj.locations_(obj.current_);
-            crtCoords = ones(1, obj.nGray)/obj.nGray*(1 - crtLocation) + ...
-                obj.axis(:)'*crtLocation;
+            crtCoords = ones(size(obj.axis))/obj.nGray*(1 - crtLocation) + ...
+                obj.axis*crtLocation;
             
             % set up the texture generator for this particular direction
             cgsStruct = [];
-            cgsStruct.(obj.group) = crtCoords;
+            for k = 1:length(obj.group)
+                cgsStruct.(obj.group{k}) = crtCoords(k, :);
+            end
             augCoords = mtc_augcoords(cgsStruct, obj.mtcs_, obj.btcDict_);
 
             for i = 1:n
@@ -164,6 +223,10 @@ classdef PatchAxisGenerator < handle
                     obj.btcMakemapsOpts_, obj.btcDict_, obj.auxOpts_, []) / ...
                     (obj.nGray - 1);
             end
+        end
+        
+        function locs = getLocations(obj)
+            locs = obj.locations_;
         end
     end
     
