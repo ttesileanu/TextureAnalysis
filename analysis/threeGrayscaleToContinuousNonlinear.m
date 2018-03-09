43bf3aae4f844ac162b91e3168321bb2ef15e02f
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-03-09 12:32:34 -0800

    Added threeGrayscaleToContinuous code to make figures for APS.

--- analysis/threeGrayscaleToContinuousNonlinear.m
+++ analysis/threeGrayscaleToContinuousNonlinear.m
@@ -137,7 +137,7 @@ for i = 1:length(sub_edirs)
         end
         
         thresholds_by_subject{j} = [thresholds_by_subject{j} crt_thresh_subj(:)'];
-        stdev_ratios_by_subject{j} = [stdev_ratios_by_subject{j} crt_stdev_ratios(:)'];
+        stdev_ratios_by_subject{j} = [stdev_ratios_by_subject{j} crt_stdev_ratios_subj(:)'];
     end
     
     groups = [groups repmat({crt_group}, 1, crt_ndirs)]; %#ok<AGROW>
@@ -670,8 +670,10 @@ for i = 1:n_unique_groups
     end
 end
 
+axis_scaling = cellfun(@(crt_axis) sqrt(3*norm(crt_axis)^2 - 1)/sqrt(2), tex_axes);
+
 hold on;
-masked_predicted_thresholds = predicted_thresholds(mask);
+masked_predicted_thresholds = axis_scaling(mask) .* predicted_thresholds(mask);
 masked_thresholds = thresholds(mask);
 for i = 1:n_unique_groups
     sub_mask_group = strcmp(masked_groups, unique_groups{i});
@@ -690,11 +692,11 @@ end
 
 thresholds_std_pos = thresholds.*(stdev_ratios - 1);
 thresholds_std_neg = thresholds - thresholds./stdev_ratios;
-h = errorbar(predicted_thresholds(mask), thresholds(mask), ...
+h = errorbar(axis_scaling(mask) .* predicted_thresholds(mask), thresholds(mask), ...
     thresholds_std_neg(mask), thresholds_std_pos(mask), ...
     'marker', 'none', 'color', [0.5 0.5 0.5], 'linestyle', 'none');
 h.CapSize = 0;
-th_h = smartscatter(predicted_thresholds(mask), thresholds(mask), 'color', threshold_colors, ...
+th_h = smartscatter(axis_scaling(mask) .* predicted_thresholds(mask), thresholds(mask), 'color', threshold_colors, ...
     'density', false);
 % smartscatter(predicted_thresholds(mask), thresholds(mask), 'color', [1 0 0], ...
 %     'density', false);
@@ -707,7 +709,7 @@ axis equal;
 
 % drawfitline(predicted_thresholds(mask), thresholds(mask), 'showci', false, ...
 %     'corrtype', 'spearman');
-drawfitline(predicted_thresholds(mask), thresholds(mask), 'line', [1 0], ...
+drawfitline(axis_scaling(mask) .* predicted_thresholds(mask), thresholds(mask), 'line', [1 0], ...
     'style', {'--k'}, 'legendloc', 'northwest');
 xlabel('Predicted thresholds');
 ylabel('Actual thresholds');
@@ -716,7 +718,7 @@ for i = 1:length(thresholds)
     if ~mask(i)
         continue;
     end
-    text(predicted_thresholds(i)+0.01, thresholds(i), ...
+    text(axis_scaling(i) * predicted_thresholds(i)+0.01, thresholds(i), ...
         groups{i}, ...
         'fontsize', 6);        
 %         [groups{i} '[' arrayfun(@int2str, tex_axes{i}) ']'], ...
@@ -725,9 +727,11 @@ end
 beautifygraph;
 preparegraph;
 
-safe_print(fullfile('figs', 'G3vsContContrastAdapt', ['cont_pred_thresholds' save_tag]));
+% safe_print(fullfile('figs', 'G3vsContContrastAdapt', ['cont_pred_thresholds' save_tag]));
 
 %% Compare predicted to actual thresholds, centroids and ellipses, by subject
+
+axis_scaling = cellfun(@(crt_axis) sqrt(3*norm(crt_axis)^2 - 1)/sqrt(2), tex_axes);
 
 mask_by_subject = cell(size(subjects));
 all_groups = [];
@@ -750,21 +754,22 @@ n_prism = size(unique(prism, 'rows'), 1);
 prism_colors = prism(n_prism);
 n_lines = size(unique(lines, 'rows'), 1);
 line_colors = lines(n_lines);
-if n_unique_groups <= n_prism
-    group_colors = prism_colors(1:n_unique_groups, :);
-elseif n_unique_groups <= n_prism + n_lines
-    group_colors = [prism_colors ; ...
-                    line_colors(1:(n_unique_groups - n_prism), :)];
-else
-    group_colors = parula(n_unique_groups);
-end
-group_markers = {{'+', 80}, {'o', 80}, {'*', 80}, {'.', 300}, ...
-    {'x', 80}, {'s', 80}, {'d', 80}, {'^', 80}, {'v', 80}, ...
-    {'>', 80}, {'<', 80}, {'p', 80}, {'h', 80}};
+% if n_unique_groups <= n_prism
+%     group_colors = prism_colors(1:n_unique_groups, :);
+% elseif n_unique_groups <= n_prism + n_lines
+%     group_colors = [prism_colors ; ...
+%                     line_colors(1:(n_unique_groups - n_prism), :)];
+% else
+%     group_colors = parula(n_unique_groups);
+% end
+group_colors = jet(n_unique_groups);
+group_markers = {{'+', 60}, {'o', 60}, {'*', 60}, {'.', 250}, ...
+    {'x', 60}, {'s', 60}, {'d', 60}, {'^', 60}, {'v', 60}, ...
+    {'>', 60}, {'<', 60}, {'p', 60}, {'h', 60}};
 
 fig = figure;
 fig.Units = 'inches';
-fig.Position = [fig.Position(1:2) 7.5 7];
+fig.Position = [fig.Position(1:2) 5 4.5];
 
 hold on;
 
@@ -784,10 +789,10 @@ for k = 1:length(subjects)
         end
     end
 
-    masked_predicted_thresholds = predicted_thresholds(mask);
+    masked_predicted_thresholds = axis_scaling(mask) .* predicted_thresholds(mask);
     masked_thresholds = thresholds_by_subject{k}(mask);
-    all_mean_preds = zeros(n_unique_groups, 1);
-    all_mean_thresh = zeros(n_unique_groups, 1);
+    all_mean_preds = nan(n_unique_groups, 1);
+    all_mean_thresh = nan(n_unique_groups, 1);
     for i = 1:n_unique_groups
         sub_mask_group = strcmp(masked_groups, unique_groups{i});
         sub_preds = masked_predicted_thresholds(sub_mask_group);
@@ -805,7 +810,7 @@ for k = 1:length(subjects)
         
         if sum(sub_mask_group) > 1
             crt_cov = cov([sub_preds(:) sub_thresh(:)]);
-            ellipse(all_mean_preds(i), all_mean_thresh(i), crt_cov, 'color', crt_color);
+%             ellipse(all_mean_preds(i), all_mean_thresh(i), crt_cov, 'color', crt_color);
         end
         
         crt_marker = group_markers{mod(i-1, length(group_markers))+1};
@@ -864,8 +869,8 @@ t_max = max(t_max_by_subject);
 t_range = [0.9*t_min 1.1*t_max];
 
 plot(t_range, t_range, '--k');
-xlabel('Predicted thresholds');
-ylabel('Actual thresholds');
+xlabel('Predicted average thresholds');
+ylabel('Measured average thresholds');
 
 axis equal;
 xlim(t_range);
@@ -874,7 +879,8 @@ ylim(t_range);
 beautifygraph;
 preparegraph;
 
-safe_print(fullfile('figs', 'G3vsContContrastAdapt', ['cont_pred_thresholds_means_' save_tag]));
+% safe_print(fullfile('figs', 'G3vsContContrastAdapt', ['cont_pred_thresholds_means_' save_tag]));
+safe_print(fullfile('figs', 'g3_via_cont_pred_means'), 'png');
 
 %% Compare predicted to actual thresholds, separate fits by order
 
@@ -1183,6 +1189,129 @@ end
 preparegraph;
 
 safe_print(fullfile('figs', 'G3vsCont', ['cont_ellipses' save_tag]));
+
+%% Make plots for subset of texture groups
+
+fig = figure;
+fig.Units = 'inches';
+fig.Position = [1 1 11 4];
+
+unique_groups = {'AB_1_1', 'AB_1_2'};
+
+max_t2 = sqrt(3)/2;
+angle_range = linspace(0, 2*pi, 100);
+
+for i = 1:length(unique_groups)
+    ax = axes;
+%     py = 0;
+    px = i-1;
+    ax.Units = 'normalized';
+    ax.OuterPosition = [px/length(unique_groups), 0, 1/length(unique_groups), 1];
+    
+    hold on;
+    
+    % draw circles for orientation (radius 1 and 1/2)
+    plot(cos(angle_range), sin(angle_range), ':', 'color', [0.4 0.4 0.4]);
+    plot(0.5*cos(angle_range), 0.5*sin(angle_range), ':', 'color', [0.4 0.4 0.4]);
+    
+    % draw the probability triangle
+    plot([-1/2 1 -1/2 -1/2], [-max_t2 0 max_t2 -max_t2], 'color', [0.5 0.7 1]);
+    
+    % draw the main axes
+    plot([0 1.5], [0 0], ':', 'color', [1 0.6 0.6], 'linewidth', 1);
+    plot([0 -1/2*1.5], [0 1.5*max_t2], ':', 'color', [1 0.6 0.6], 'linewidth', 1);
+    plot([0 -1/2*1.5], [0 -1.5*max_t2], ':', 'color', [1 0.6 0.6], 'linewidth', 1);
+    
+    % label the corners
+    text(1.07, -0.1, '[0,1,0]', 'fontsize', 12);
+    text(-1.1,  max_t2+0.05, '[0,0,1]', 'fontsize', 12);
+    text(-1.1, -max_t2-0.01, '[1,0,0]', 'fontsize', 12);
+    
+    % draw measured and predicted thresholds
+    mask_idxs = find(strcmp(groups, unique_groups{i}));
+    measured_points = [];
+    predicted_points = [];
+    h_meas = [];
+    h_pred = [];
+    for j = 1:length(mask_idxs)
+        crt_idx = mask_idxs(j);
+        crt_axis = tex_axes{crt_idx};
+        
+        % measured threshold
+        crt_threshold = thresholds(crt_idx);
+        crt_thresh_pos = [1/3 1/3 1/3]*(1 - crt_threshold) + crt_axis*crt_threshold;
+                
+        % predicted threshold
+        crt_predicted_threshold = predicted_thresholds(crt_idx);
+        crt_pred_thresh_pos = [1/3 1/3 1/3]*(1 - crt_predicted_threshold) + ...
+            crt_axis*crt_predicted_threshold;
+        
+        % this is a litle roundabout, but easier conceptually
+        crt_t1 = (3*crt_thresh_pos(2) - 1)/2;
+        crt_t2 = (crt_thresh_pos(3) - crt_thresh_pos(1)) * max_t2;
+        
+        crt_pred_t1 = (3*crt_pred_thresh_pos(2) - 1)/2;
+        crt_pred_t2 = (crt_pred_thresh_pos(3) - crt_pred_thresh_pos(1)) * max_t2;
+        
+        % store points for fitting ellipse
+        if ~any(isnan([crt_t1 crt_t2]))
+            measured_points = [measured_points ; [crt_t1 crt_t2]]; %#ok<AGROW>
+        end
+        if ~any(isnan([crt_pred_t1 crt_pred_t2]))
+            predicted_points = [predicted_points ; [crt_pred_t1 crt_pred_t2]]; %#ok<AGROW>
+        end
+        
+        % plot
+        h_meas = [h_meas plot(crt_t1, crt_t2, 'kx', 'linewidth', 1)]; %#ok<AGROW>
+        h_pred = [h_pred plot(crt_pred_t1, crt_pred_t2, 'rx', 'linewidth', 1)]; %#ok<AGROW>
+    end
+    
+    legend([h_meas(1) h_pred(1)], 'measurements', 'predictions');
+    
+    % show ellipses
+%     if size(measured_points, 1) > 2
+%         measured_M = fit_ellipse(measured_points);
+%         [crt_V, crt_D] = eig(measured_M);
+%         crt_D = diag(crt_D);
+%         crt_D(crt_D < 0 & crt_D > -1e-4) = 0;
+%         if all(crt_D >= 0)
+%             crt_y = [1/sqrt(crt_D(1))*cos(angle_range(:)) ...
+%                 1/sqrt(crt_D(2))*sin(angle_range(:))]';
+%             crt_x = crt_V*crt_y;
+%             plot(crt_x(1, :), crt_x(2, :), 'color', [0.5 0.5 0.5]);
+%         end
+%     end
+%     if size(predicted_points, 1) > 2
+%         predicted_M = fit_ellipse(predicted_points);
+%         [crt_V, crt_D] = eig(predicted_M);
+%         crt_D = diag(crt_D);
+%         crt_D(crt_D < 0 & crt_D > -1e-4) = 0;
+%         if all(diag(crt_D) >= 0)
+%             crt_y = [1/sqrt(crt_D(1))*cos(angle_range(:)) ...
+%                 1/sqrt(crt_D(2))*sin(angle_range(:))]';
+%             crt_x = crt_V*crt_y;
+%             plot(crt_x(1, :), crt_x(2, :), 'color', [1 0.6 0.6]);
+%         end
+%     end
+    
+    % arrange plot sizes and labels
+    axis equal;
+    
+    xlim([-1.5 1.5]);
+    ylim([-1.5 1.5]);
+    
+%     xlim([-2 2]);
+%     ylim([-2 2]);
+    
+    title(unique_groups{i});
+    
+%    beautifygraph;
+    set(gca, 'box', 'on', 'xminortick', 'on', 'yminortick', 'on', 'linewidth', 1);
+end
+
+preparegraph;
+
+safe_print(fullfile('figs', 'g3_via_cont_match_mismatch'));
 
 %% Check how much thresholds change with noise radius
 
