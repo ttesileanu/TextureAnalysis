0b72556e5cf9932776bdbbf44e86bf755f345655
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-04-06 20:35:41 -0400

    Added support ofr multi-group directions to matchThresholds.

--- misc/matchThresholds.m
+++ misc/matchThresholds.m
@@ -16,14 +16,15 @@ for i = 1:length(target_groups)
     crt_group = target_groups{i};
     crt_direction = target_directions{i};
     mask = strcmp(groups, crt_group) & ...
-        cellfun(@(v) norm(v - crt_direction) < tol, directions);
+        cellfun(@(v) isequal(size(v), size(crt_direction)) && norm(v - crt_direction) < tol, ...
+        directions);
     if sum(mask) == 0
         error([mfilename ':nomatch'], ['No match found for group ' crt_group ', direction ' num2str(crt_direction) '.']);
     end
-    if sum(mask) > 1
-        error([mfilename ':multimatch'], ['Multiple matches found for group ' crt_group ', direction ' num2str(crt_direction) '.']);
+    if sum(mask) > 1 && std(thresholds(mask)) > tol
+        warning([mfilename ':multimatch'], ['Multiple distinct matches found for group ' crt_group ', direction ' num2str(crt_direction) '. Using the first one.']);
     end
-    target_thresholds(i) = thresholds(mask);
+    target_thresholds(i) = thresholds(find(mask, 1));
 end
 
 end
