44c730c00b2d2c368271c4832070ad31df8898c7
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-06-19 14:00:23 -0400

    Starting with a clean slate.

--- preprocess/quantize.m
+++
@@ -1,9 +0,0 @@
-function img = quantize(img, n)
-% QUANTIZE Quantize images to discrete color levels.
-%   imgOut = QUANTIZE(img, n) discretizes the matrix `img` to `n` levels,
-%   equally spaced between 0 and 1. Values outside the [0, 1] range are
-%   clipped to 0 or 1.
-
-img = min(max(floor(n*img)/(n-1), 0), 1);
-
-end
--- preprocess/stochasticBinarize.m
+++
@@ -1,9 +0,0 @@
-function img = stochasticBinarize(img)
-% stochasticBinarize Binarize images using a stochastic approach.
-%   imgOut = stochasticBinarize(img) binarizes the matrix `img` by using a
-%   stochastic approach: a pixel will be turned white (i.e., equal to 1)
-%   with probability given by the initial value of the pixel, `img(i, j)`.
-
-img = 1*(rand(size(img)) < img);
-
-end
