bab86289cba606888b8060fdba10a12d4a08f631
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-06-21 13:55:32 -0400

    Added image equalization and quantization routines.

---
+++ preprocess/quantizeImage.m
@@ -0,0 +1,13 @@
+function [img, crop] = quantizeImage(img, n)
+% quantizeImage Quantize images to discrete color levels.
+%   imgOut = quantizeImage(img, n) discretizes the matrix `img` to `n`
+%   levels, equally spaced between 0 and 1. Values outside the [0, 1] range
+%   are clipped to 0 or 1.
+%
+%   [imgOut, crop] = quantizeImage(...) returns a cropping area, always
+%   equal to [1 1 size(img)].
+
+img = min(max(floor(n*img)/(n-1), 0), 1);
+crop = [1 1 size(img)];
+
+end
