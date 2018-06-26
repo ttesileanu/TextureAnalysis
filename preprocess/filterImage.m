212221aa4fa00066600a8ad9be89587f8786d3fd
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-06-26 19:01:47 -0400

    Updated filterImage to use the new patchification architecture.

--- preprocess/filterImage.m
+++ preprocess/filterImage.m
@@ -1,63 +1,44 @@
-function [image, crop] = filterImage(image, filter, method)
-% filterImage Apply a filter to the image, either on a patch-by-patch basis
-% or to the full image at once.
+function [image, crop] = filterImage(image, filter)
+% filterImage Apply a filter to a 2d or 3d (patchified) array.
 %   filtered = filterImage(image, filter) applies the given filter to the
-%   image. The filter must be given as a matrix, and will be applied by
-%   first splitting the image into non-overlapping patches of size
-%   `size(filter)`, and then filtering each patch. The right and bottom
-%   edges of the image are clipped if the image size is not an integer
-%   multiple of the filter size.
-%
-%   filterImage(image, filter, 'full') applies the filter to the whole
-%   image instead. An edge of size equal to half the size of the filter is
-%   clipped on all four sides of the image to remove edge effects.
+%   image. If the `filter` matrix is the same size as the first two
+%   dimensions of `image`, it is applied to each one of the patches
+%   identified by the third dimension. If `image` is 2d and has a different
+%   size than the `filter`, then after the filte is applied, an edge of
+%   size equal to half the size of the filter is clipped on all four sides
+%   of the image to remove edge effects.
 %
 %   [filtered, crop] = filterImage(...) also returns a vector of four
 %   elements [row1, col1, row2, col2] identifying the range in the original
-%   image corresponding to the filtered image.
+%   image corresponding to the filtered image, in the case in which the
+%   input image was 2d.
 
-if nargin < 3
-    method = 'patch';
+imSize = size(image);
+
+if length(imSize) == 2
+    crop = [1 1 imSize];
+    nPatches = 1;
+else
+    crop = [];
+    nPatches = imSize(3);
 end
 
-crop = [1 1 size(image)];
-
-switch method
-    case 'patch'
-%         patchifier = ImagePatchifier({size(image)}, size(filter));
-%         nPatches = patchifier.gridSize;
-%         while patchifier.next
-%             crtCoords = patchifier.getPatchCoordinates;
-%             rows = crtCoords(1):crtCoords(3);
-%             cols = crtCoords(2):crtCoords(4);
-%             image(rows, cols) = real(ifft2(fft2(image(rows, cols)) .* filter));
-%         end
-        
-        % XXX the manual version is about 15-20% faster...
-        nPatches = floor(size(image) ./ size(filter));
-        for i = 1:nPatches(1)
-            ys = 1 + (i-1)*size(filter, 1);    % patch starts here
-            rows = (ys:ys+size(filter, 1)-1);  % all patch rows
-            for j = 1:nPatches(2)
-                xs = 1 + (j-1)*size(filter, 2);   % patch starts here
-                cols = (xs:xs+size(filter, 2)-1); % all patch columns
-                image(rows, cols) = real(ifft2(fft2(image(rows, cols)) .* filter));
-            end
-        end
-        
-        % trim overflow
-        crop(3:4) = nPatches .* size(filter);
-        image = image(1:crop(3), 1:crop(4));
-    case 'full'
-        trimEdge = floor(size(filter)/2);
-        cShift = trimEdge - 1;
-        image = imfilter(image, circshift(ifft2(filter), cShift));
-        
-        crop(1:2) = trimEdge;
-        crop(3:4) = size(image) - trimEdge + 1;
-        image = image(crop(1):crop(3), crop(2):crop(4));
-    otherwise
-        error([mfilename ':baderror'], 'Unrecognized filtering method.');
+if isequal(imSize(1:2), size(filter))
+    for i = 1:nPatches
+        image(:, :, i) = real(ifft2(fft2(image(:, :, i)) .* filter));
+    end
+else
+    if length(imSize) ~= 2
+        error([mfilename ':badsz'], 'Patchifed images require the filter size to match the patch size.');
+    end
+    
+    trimEdge = floor(size(filter)/2);
+    cShift = trimEdge - 1;
+    image = imfilter(image, circshift(ifft2(filter), cShift));
+    
+    crop(1:2) = trimEdge;
+    crop(3:4) = imSize - trimEdge + 1;
+    image = image(crop(1):crop(3), crop(2):crop(4));
 end
 
 end
