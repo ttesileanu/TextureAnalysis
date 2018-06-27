4fdd48e9d22cbc4dd4001496428a3cd6c7fecb10
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-06-27 16:00:45 -0400

    Updated generateTextureDistribution to use the new patchification architecture.

--- batch/generateTextureDistribution.m
+++ batch/generateTextureDistribution.m
@@ -1,40 +1,28 @@
-function results = generateTextureDistribution(images, nLevels, patchSize, varargin)
+function results = generateTextureDistribution(images, nLevels, varargin)
 % generateTextureDistribution Calculate texture statistics for a set of
 % images.
-%   results = generateTextureDistribution(images, nLevels, patchSize)
-%   calculates texture statistics with `nLevels` gray levels and the given
-%   `patchSize` for the images in `images` (which can be in any of the
-%   formats accepted by walkImages). Note that the texture analysis routine
-%   assumes that the preprocessing routines (see 'preprocessing' option
-%   below) convert the image to a format in which all pixels take `nLevels`
-%   discrete values in the interval [0, 1] (except in the case in which
-%   `nLevels` is infinite; see analyzeTexture).
+%   results = generateTextureDistribution(images, nLevels) calculates
+%   texture statistics with `nLevels` gray levels for the images in
+%   `images` (which can be in any of the formats accepted by walkImages).
+%   Note that the texture analysis routine assumes that the preprocessing
+%   routines (see 'preprocessing' option below) split the images in
+%   appropriately-sized patches (if necessary), and convert them to a
+%   format in which all pixels take `nLevels` discrete values in the
+%   interval [0, 1] (except in the case in which `nLevels` is infinite; see
+%   analyzeTexture).
 %
-%   results = generateTextureDistribution(images, 'masks', masks) uses the
-%   images given in the cell array `masks` to identify portions of the
-%   image on which to focus the analysis (see XXX for details).
-%   Empty entries in the `masks` cell array can be used to skip some of the
-%   images in the image set.
+%   results = generateTextureDistribution(..., 'masks', masks) uses the
+%   images given in the cell array `masks` to identify portions of each
+%   input image on which to focus the analysis (see getImageTexturesByObject
+%   for details). Empty entries in the `masks` cell array can be used to
+%   skip some of the images in the image set.
 %
 %   Options:
-%    'stride': scalar or pair of numbers
-%       If set, use a sliding patch instead of being restricted to
-%       overlapping patches. See `ImagePatchifier`.
 %    'masks': cell array of matrices
 %       The images in this cell array are used to identify portions of the
-%       image on which to focus the analysis (see XXX for details).
-%       Empty entries in the `masks` cell array can be used to skip some of
-%       the images in the image set.
-%    'maxPatchesPerImage': integer
-%       If given, this ensures that the number of patches per image doesn't
-%       exceed a given value. If an image generates more patches than
-%       `maxPatchesPerImage`, random sampling without replacement is used
-%       to select `maxPatchesPerImage` of them to keep.
-%       (default: no maximum)
-%    'minPatchUsed': double
-%       Minimum fraction of patch that needs to be available (e.g.,
-%       contained within a region identified by the masks) to be analyzed.
-%       See XXX.
+%       input images on which to focus the analysis (see
+%       getImageTexturesByObject for details). Images corresponding to
+%       empty entries in the `masks` cell array are skipped.
 %    'covariances': logical
 %       Set to true (default) to evaluate covariance matrices for the
 %       patches. If no masks are provided, a single covarince matrix is
@@ -46,7 +34,7 @@ function results = generateTextureDistribution(images, nLevels, patchSize, varar
 %       each image, in the same format as for walkImages.
 %    Any other key-value arguments are directly passed to walkImages.
 %
-%   See also: walkImages, analyzeTexture, ImagePatchifier.
+%   See also: walkImages, analyzeTexture, patchify, getImageTextures, getImageTexturesByObject.
 
 % parse optional arguments
 parser = inputParser;
@@ -55,10 +43,6 @@ parser.FunctionName = mfilename;
 parser.KeepUnmatched = true;
 
 parser.addParameter('masks', {}, @(c) iscell(c));
-parser.addParameter('stride', [], @(v) isempty(v) || ...
-    (isnumeric(v) && isvector(v) && ismember(length(v), [1, 2]) && all(v > 0)));
-parser.addParameter('maxPatchesPerImage', inf, @(n) isnumeric(n) && isscalar(n) && n >= 0);
-parser.addParameter('minPatchUsed', 0, @(n) isnumeric(n) && isscalar(n) && n >= 0 && n <= 1);
 parser.addParameter('covariances', true, @(b) islogical(b) && isscalar(b));
 parser.addParameter('preprocessing', {}, @(c) iscell(c) && (isempty(c) || isvector(c)));
 
@@ -74,10 +58,6 @@ parser.parse(varargin{:});
 params = parser.Results;
 unmatched = parser.Unmatched;
 
-% set up the arguments to pass to getImageTexturesByObject
-analysisArgs = {'patchSize', patchSize, 'stride', params.stride, ...
-    'maxPatchesPerImage', params.maxPatchesPerImage, 'minPatchUsed', params.minPatchUsed};
-
 % process the images
 masks = params.masks;
 results = walkImages([params.preprocessing {@walker}], images, unmatched);
@@ -92,12 +72,6 @@ if params.covariances
             safeCov(results.ev(results.objIds == crtObj)), 1:length(allObjs));
     end
 end
-
-% keep track of the options used to generate the texture distribution
-results.options.patchSize = patchSize;
-results.options.stride = params.stride;
-results.options.maxPatchesPerImage = params.maxPatchesPerImage;
-results.options.minPatchUsed = params.minPatchUsed;
 
     function crtRes = walker(i, image, crop)
         % walker(i, image, crop) processes the image and returns texture
@@ -118,8 +92,7 @@ results.options.minPatchUsed = params.minPatchUsed;
         
         % calculate statistics
         try
-            crtRes = getImageTexturesByObject(image, nLevels, mask, analysisArgs{:}, ...
-                'maskCrop', crop);
+            crtRes = getImageTexturesByObject(image, nLevels, mask, 'maskCrop', crop);
         catch errMsg
             newErrMsg.message = ['Image #' int2str(i) ': ' errMsg.message];
             newErrMsg.identifier = errMsg.identifier;
