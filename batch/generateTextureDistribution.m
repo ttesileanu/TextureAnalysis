83b57f8906073c8f4024a199bd36585b60db1987
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-06-28 13:57:35 -0400

    Updated generateTextureDistribution to work with 3d patchified images. Also removed support for masks / object IDs.

--- batch/generateTextureDistribution.m
+++ batch/generateTextureDistribution.m
@@ -11,30 +11,16 @@ function results = generateTextureDistribution(images, nLevels, varargin)
 %   interval [0, 1] (except in the case in which `nLevels` is infinite; see
 %   analyzeTexture).
 %
-%   results = generateTextureDistribution(..., 'masks', masks) uses the
-%   images given in the cell array `masks` to identify portions of each
-%   input image on which to focus the analysis (see getImageTexturesByObject
-%   for details). Empty entries in the `masks` cell array can be used to
-%   skip some of the images in the image set.
-%
 %   Options:
-%    'masks': cell array of matrices
-%       The images in this cell array are used to identify portions of the
-%       input images on which to focus the analysis (see
-%       getImageTexturesByObject for details). Images corresponding to
-%       empty entries in the `masks` cell array are skipped.
-%    'covariances': logical
-%       Set to true (default) to evaluate covariance matrices for the
-%       patches. If no masks are provided, a single covarince matrix is
-%       calculated, averaging over all patches. If there are masks, then a
-%       covariance matrix is calculated for every object ID found in the
-%       masks.
+%    'covariance': logical
+%       Set to true (default) to evaluate a covariance matrix for the
+%       patches.
 %    'preprocessing':
 %       A cell array of preprocessing functions that will be applied to
 %       each image, in the same format as for walkImages.
 %    Any other key-value arguments are directly passed to walkImages.
 %
-%   See also: walkImages, analyzeTexture, patchify, getImageTextures, getImageTexturesByObject.
+%   See also: walkImages, analyzeTexture, patchify, getImageTextures.
 
 % parse optional arguments
 parser = inputParser;
@@ -42,8 +28,7 @@ parser.CaseSensitive = true;
 parser.FunctionName = mfilename;
 parser.KeepUnmatched = true;
 
-parser.addParameter('masks', {}, @(c) iscell(c));
-parser.addParameter('covariances', true, @(b) islogical(b) && isscalar(b));
+parser.addParameter('covariance', true, @(b) islogical(b) && isscalar(b));
 parser.addParameter('preprocessing', {}, @(c) iscell(c) && (isempty(c) || isvector(c)));
 
 % defaults
@@ -59,40 +44,20 @@ params = parser.Results;
 unmatched = parser.Unmatched;
 
 % process the images
-masks = params.masks;
 results = walkImages([params.preprocessing {@walker}], images, unmatched);
 
-% generate covariances, if requested
-if params.covariances
+% generate covariance, if requested
+if params.covariance
     results.covM = safeCov(results.ev);
-    if ~isempty(params.masks)
-        % this assumes that object IDs are consistent across images!
-        allObjs = unique(results.objIds);
-        results.covPerObj = cellfun(@(crtObj) ...
-            safeCov(results.ev(results.objIds == crtObj)), 1:length(allObjs));
-    end
 end
 
     function crtRes = walker(i, image, crop)
         % walker(i, image, crop) processes the image and returns texture
         % statistics data for it.
         
-        if isempty(masks)
-            % there is no mask
-            mask = [];
-        else
-            % there are masks
-            mask = masks{i};
-            % if this particular mask is empty, skip image
-            if isempty(mask)
-                crtRes = [];
-                return;
-            end
-        end
-        
         % calculate statistics
         try
-            crtRes = getImageTexturesByObject(image, nLevels, mask, 'maskCrop', crop);
+            crtRes = getImageTextures(image, nLevels);
         catch errMsg
             newErrMsg.message = ['Image #' int2str(i) ': ' errMsg.message];
             newErrMsg.identifier = errMsg.identifier;
@@ -105,6 +70,7 @@ end
             return;
         end
         % keep track of the image from which the patches originated
+        crtRes.patchLoactions = crop;
         crtRes.imgIds = i*ones(size(crtRes.ev, 1), 1);
     end
 
