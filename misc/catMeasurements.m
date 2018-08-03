7dc7685f8be6ebc1803dbd60af18066e30ea317b
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-08-03 16:08:03 -0400

    Fix problem with concatenating measurements with different fields

--- misc/catMeasurements.m
+++ misc/catMeasurements.m
@@ -18,28 +18,28 @@ for k = 1:length(allFields)
     field = allFields{k};
     measurements.(field) = [];
     for i = 1:nargin
-        if isempty(measurements.(field))
-            if isfield(varargin{i}, field)
-                measurements.(field) = varargin{i}.(field);
-            end
+        nCrt = length(varargin{i}.groups);
+        if isfield(varargin{i}, field)
+            value = varargin{i}.(field);
         else
-            if isfield(varargin{i}, field)
-                measurements.(field) = [measurements.(field) ; varargin{i}.(field)];
-            else
-                switch field
-                    case {'thresholdIntervals', 'nSubjects'}
-                        measurements.(field) = [measurements.(field) ; ...
-                            nan(length(varargin{i}.groups), size(measurements.(field), 2))];
-                    case 'subjects'
-                        measurements.(field) = [measurements.(field) ; ...
-                            repmat({''}, length(varargin{i}.groups), 1)];
-                    case 'multi'
-                        measurements.(field) = [measurements.(field) ; ...
-                            cellfun(@(g) 1 + sum(g == ';'), varargin{i}.groups(:))];
-                    otherwise
-                        error([mfilename ':badfield'], 'Don''t know how to fill in default values for %s.', field);
-                end
+            switch field
+                case 'thresholdIntervals'
+                    value = nan(nCrt, 2);
+                case 'nSubjects'
+                    value = nan(nCrt, 1);
+                case 'subjects'
+                    value = repmat({''}, nCrt, 1);
+                case 'multi'
+                    value = (cellfun(@(g) sum(g == ';'), varargin{i}.groups(:)) > 0);
+                otherwise
+                    error([mfilename ':badfield'], 'Don''t know how to fill in default values for %s.', field);
             end
+        end
+        
+        if isempty(measurements.(field))
+            measurements.(field) = value;
+        else
+            measurements.(field) = [measurements.(field) ; value];
         end
     end
 end
