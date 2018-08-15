5d2d1323a6af076a48ac9d6b5f32d488a45ec546
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-08-15 17:30:05 -0400

    Add scripts for multi-cutoff analysis

---
+++ analysis/generateTernaryNIDistributionMultiCutoffs.m
@@ -0,0 +1,108 @@
+% analyze natural images after ternarizing to generate a distribution over
+% ternary textures
+
+%% Setup
+
+% This script's behavior can be modified by defining some variables before
+% running. When these variables are not defined, default values are used.
+%
+% Options:
+%   valuesNR
+%       Cell array of pairs [N, R] of downsampling factor and patch size
+%       for which to run the analysis.
+%   cutoffChoices
+%       Choices to use for the quantization cutoffs.
+
+setdefault('valuesNR', {[2, 32]});
+
+defaultCutoffs0 = arrayfun(@(g) [(1-g)/2, (1+g)/2], linspace(0, 1, 25), 'uniform', false);
+defaultCutoffs = cutoffs0(2:end-1);
+setdefault('cutoffChoices', defaultCutoffs);
+
+%% Preprocess options
+
+valuesN = cellfun(@(x) x(1), valuesNR);
+valuesR = cellfun(@(x) x(2), valuesNR);
+
+%% Load the filters
+
+filters = cell(1, length(valuesN));
+for i = 1:length(valuesN)
+    N = valuesN(i);
+    R = valuesR(i);
+    filterFilename = fullfile('filters', ['filter' int2str(N) 'x' int2str(R) '.mat']);
+    % load the filter from whichever variable has the right size and type
+    crtFilter = open(filterFilename);
+    fields = fieldnames(crtFilter);
+    isfilter = @(f) isnumeric(f) && ismatrix(f) && all(size(f) == [R R]);
+    valid = cellfun(@(s) isfilter(crtFilter.(s)), fields);
+    if sum(valid) == 0
+        error(['Can''t find valid filter data in ' filterFilename '.']);
+    elseif sum(valid) > 1
+        error(['Don''t know which variable to use from ' filterFilename '.']);
+    end
+    filterField = fields{valid};
+    filters{i} = crtFilter.(filterField);
+end
+
+%% Generate the ternary stats
+
+images = parseImageNameFile('PennNoSkyIndex.txt', 'NaturalImages');
+
+tic;
+for i = 1:length(valuesN)
+    N = valuesN(i);
+    R = size(filters{i}, 1);
+    disp(['Working on ternary stats at N=' int2str(N) ', R=' int2str(R) ' (' int2str(i) ...
+        '/' int2str(length(valuesN)) ')...']);
+    
+    sharpness = [];
+    for k = 1:length(cutoffChoices)
+        crtCutoffs = cutoffChoices{k};
+        
+        disp(['...cutoffs [' num2str(crtCutoffs, '%6.2f') '], time = ' num2str(toc, '%.2f') ' seconds...']);
+        
+        % set up the preprocessing pipeline
+        preprocessPipeline = {
+            @logTransform, ...
+            @(image) blockAverage(image, N), ...
+            @(image) patchify(image, R), ...
+            @(image) filterImage(image, filters{i}), ...
+            @(image) equalizeImage(image, 'jitter', 0, 'minLevels', 64), ...
+            @(image) quantizeImage(image, 3, 'cutoffs', crtCutoffs)};
+        
+        % image quantization has a small random component that matters for
+        % patches that have many identical pixel values
+        % to keep things reproducible, we fix the random number generator seed
+        rng('default');
+        results = generateTextureDistribution(images, 3, 'preprocessing', preprocessPipeline);
+        
+        % perform focus analysis
+        disp(['Working on focus analysis for ternary results ' int2str(i) ...
+            '/' int2str(length(results)) ', cutoffs [' num2str(crtCutoffs, '%6.2f') ']...']);
+        % focus analysis has a random component
+        % to keep things reproducible, we fix the random number generator seed
+        rng('default');
+        try
+            if isempty(sharpness)
+                results = runFocusAnalysis(results);
+                sharpness = results.focus.sharpness;
+            else
+                results.focus.sharpness = sharpness;
+                results = runFocusAnalysis(results, 'reuseSharpness', true);
+            end
+        catch me
+            warning(['Focus analysis failed, message: ' me.message]);
+        end
+        
+        % save
+        disp('Saving to file...');
+        filename = ['TernaryDistribution_PennNoSky_' int2str(N) 'x' int2str(R) ...
+            '_cutoff_' int2str(round(100*crtCutoffs(1))) '_' int2str(round(100*crtCutoffs(2))) '.mat'];
+        
+        cutoffs = crtCutoffs;
+        
+        save(fullfile('save', 'multicutoff', filename), 'results', 'N', 'R', 'cutoffs');
+    end
+end
+disp(['Generating the ternary stats took ' num2str(toc, '%.2f') ' seconds.']);
---
+++ analysis/generateTernaryNIPredictionsMultiCutoffs.m
@@ -0,0 +1,103 @@
+% calculate predictions and prediction accuracy as a function of
+% ternarization cut points
+
+%% Setup
+
+% This script's behavior can be modified by defining some variables before
+% running. When these variables are not defined, default values are used.
+%
+% Options:
+%   restrictToFocus
+%       Set to `true` to only keep patches that were identified as in-focus
+%       by a two-Gaussian fit.
+
+setdefault('restrictToFocus', true);
+
+%% Load the psychophysics data
+
+pp = loadTernaryPP(fullfile('data', 'mtc_soid_xlsrun_summ.mat'));
+
+% add additional data from Jonathan, but keep only AC_1_2 plane
+pp_extra = open('data/extra_ternary_thresholds.mat');
+pp_extra_AC12 = selectMeasurements(pp_extra.avg, ...
+    strcmp(pp_extra.avg.groups, 'AC_1_2'));
+
+pp = catMeasurements(pp, pp_extra_AC12);
+
+%% Load ternary distributions, preprocess, and get predictions
+
+allPredictionResults = struct;
+
+allFiles = dir(fullfile('save', 'multicutoff'));
+
+crt = 1;
+for i = 1:length(allFiles)
+    crtFile = allFiles(i);
+    
+    % keep only files (not folders) ending in .mat
+    if crtFile.isdir
+        continue;
+    end
+    
+    % XXX should restrict to a common prefix
+    crtFile = fullfile(crtFile.folder, crtFile.name);
+    [~, ~, crtExt] = fileparts(crtFile);
+    if ~strcmp(crtExt, '.mat')
+        continue;
+    end
+    
+    disp(['Processing ' crtFile '...']);
+    
+    % load data, restrict to in-focus patches if necessary
+    crtData = open(crtFile);
+    crtCutoffs = crtData.cutoffs;
+    if isfield(crtData.results, 'focus')
+        crtResults = rmfield(crtData.results, 'focus');
+    else
+        crtResults = crtData.results;
+    end
+    if restrictToFocus
+        if ~isfield(crtData.results, 'focus') || ~isfield(crtData.results.focus, 'clusterIds')
+            warning('%s does not contain focus information. Skipping.', crtFile);
+            continue;
+        end
+        mask = (crtData.results.focus.clusterIds == crtData.results.focus.focusCluster);
+        fields = {'ev', 'patchLocations', 'imageIds'};
+        for j = 1:length(fields)
+            crtResults.(fields{j}) = crtResults.(fields{j})(mask, :);
+        end
+        crtResults.covM = cov(crtResults.ev);
+    end
+    
+    % use only second-order groups to set the overall scaling of the predictions
+    [crtGain, crtPredictions, crtPredictionDetails] = getPredictionsFromTernaryStats(...
+        crtResults.ev, pp, 'fitScaleOptions', {'mask', cellfun(@length, pp.groups) == 6});
+    
+    % store the results
+    allPredictionResults(crt).file = crtFile;
+    allPredictionResults(crt).cutoffs = crtCutoffs;
+    allPredictionResults(crt).gain = crtGain;
+    allPredictionResults(crt).predictions = crtPredictions;
+    allPredictionResults(crt).predictionDetails = crtPredictionDetails;
+    crt = crt + 1;
+end
+
+% sort the prediction results in increaing order of cutoffs(1)
+[~, reorder] = sort(arrayfun(@(s) s.cutoffs(1), allPredictionResults));
+allPredictionResults = allPredictionResults(reorder);
+
+%% Save the results
+
+save(fullfile('save', 'TernaryDistribution_PennNoSky_2x32_mutlicutoff_predictions.mat'));
+
+%% Check how match to psychophysics depends on cutoff
+
+grayAmounts = arrayfun(@(s) diff(s.cutoffs), allPredictionResults);
+% restrict comparison to either second-order or mixed groups
+groupMaskFct = @(g) length(g) == 6 || sum(g == ';') == 1;
+differences = arrayfun(@(s) compareMeasurements(s.predictions, pp, 'group', ...
+    'groupMaskFct', groupMaskFct), allPredictionResults);
+
+plot(grayAmounts, differences, '.-');
+xlabel('Fraction of gray');
+ylabel('Mismatch between NI and PP');
