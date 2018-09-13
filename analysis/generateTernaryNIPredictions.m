5f6e1f1c2ad44cc0c4f0469b060488bdb5d0da0e
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-09-13 12:30:29 -0400

    Use gain squaring when generating predictions

--- analysis/generateTernaryNIPredictions.m
+++ analysis/generateTernaryNIPredictions.m
@@ -22,11 +22,28 @@
 %   restrictToFocus
 %       Set to `true` to only keep patches that were identified as in-focus
 %       by a two-Gaussian fit.
+%   useErrorBars
+%       Set to `false` to ignore error bars when finding the optimal
+%       scaling for NI predictions.
+%   gainTransform
+%       A function to apply to the gains obtained from efficient coding.
+%       This can be either a function handle or one of
+%        'identity'
+%           The gains are kept as they are.
+%        'square'
+%           The gains are squared. This was used in Hermundstad et al.,
+%           leading to threshold predictions that are inversely proportional
+%           to natural image standard deviations instead of their square
+%           roots. Since the efficient coding problem solved here uses a
+%           Gaussian approximation, this transformation might indicate a
+%           departure of visual processing in the brain from Gaussianity.
 
 setdefault('dbChoice', 'PennNoSky');
 setdefault('compressType', 'equalize');
 setdefault('NRselection', [2, 32]);
 setdefault('restrictToFocus', true);
+setdefault('useErrorBars', true);
+setdefault('gainTransform', 'square');
 
 %% Preprocess options
 
@@ -36,6 +53,18 @@ else
     compressExt = '';
 end
 niFileName = ['TernaryDistribution_' dbChoice compressExt '.mat'];
+
+switch gainTransform
+    case 'identity'
+        gainTransformFct = @(x) x;
+    case 'square'
+        gainTransformFct = @(x) x.^2;
+    otherwise
+        if ~isfa(gainTransform, 'function_handle')
+            error('Invalid value for gainTransform.');
+        end
+        gainTransformFct = gainTransform;
+end
 
 %% Load the ternary NI distribution
 
@@ -73,17 +102,25 @@ pp = loadTernaryPP(fullfile('data', 'mtc_soid_xlsrun_summ.mat'));
 %     'subjects', '*', 'keepNaN', false);
 
 % add additional data from Jonathan, but keep only AC_1_2 plane
-pp_extra = open('data/extra_ternary_thresholds.mat');
-pp_extra_AC12 = selectMeasurements(pp_extra.avg, ...
-    strcmp(pp_extra.avg.groups, 'AC_1_2'));
-
-pp = catMeasurements(pp, pp_extra_AC12);
+% pp_extra = open('data/extra_ternary_thresholds.mat');
+% pp_extra_AC12 = selectMeasurements(pp_extra.avg, ...
+%     strcmp(pp_extra.avg.groups, 'AC_1_2'));
+% 
+% pp = catMeasurements(pp, pp_extra_AC12);
 
 %% Calculate gains and predicted thresholds
 
 % use only second-order groups to set the overall scaling of the predictions
+if useErrorBars
+    ppForFit = pp;
+else
+    ppForFit = rmfield(pp, 'thresholdIntervals');
+end
+% [gain, predictions, predictionDetails] = getPredictionsFromTernaryStats(...
+%     ni.ev, ppForFit, 'fitScaleOptions', {'mask', cellfun(@length, pp.groups) == 6});
 [gain, predictions, predictionDetails] = getPredictionsFromTernaryStats(...
-    ni.ev, pp, 'fitScaleOptions', {'mask', cellfun(@length, pp.groups) == 6});
+    ni.ev, ppForFit, 'fitScaleOptions', {'mask', cellfun(@(s) length(s) == 6 || sum(s == ';') == 1, pp.groups)}, ...
+    'efficientCodingOptions', {'gainTransform', gainTransformFct});
 
 %% Save
 
