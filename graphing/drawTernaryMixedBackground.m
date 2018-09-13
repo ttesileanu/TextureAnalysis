0f2e47b05934d6966d6924aae341395f798a15f8
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-09-13 15:37:08 -0400

    Remove useless arguments from drawTernaryMixedBackground

--- graphing/drawTernaryMixedBackground.m
+++ graphing/drawTernaryMixedBackground.m
@@ -1,13 +1,9 @@
-function drawTernaryMixedBackground(groupDir1, groupDir2)
+function drawTernaryMixedBackground
 % drawTernaryMixedBackground Draw guiding axes through origin and guiding
 % circles for pairs of ternary texture groups.
-%   drawTernaryMixedBackground(groupDir1, groupDir2) draws guiding axes
-%   going through the origin and guiding circles for a plot representing a
-%   mix of two ternary texture groups. The arguments give the group names
-%   together with the axis; for instance, a typical notation would write
-%   'AB_1_1[0]' for the [0, 0, 1] direction in the 'AB_1_1' plane. However,
-%   these are only used for labeling the axes, so any other notation can be
-%   used.
+%   drawTernaryMixedBackground draws guiding axes going through the origin
+%   and guiding circles for a plot representing a mix of two ternary
+%   texture groups.
 
 % we will overlay several graphical objects
 wasHold = ishold;
@@ -26,10 +22,6 @@ end
 % draw the main axes
 plot([-1 1], [0 0], ':', 'color', [1 0.6 0.6], 'linewidth', 1);
 plot([0 0], [-1 1], ':', 'color', [1 0.6 0.6], 'linewidth', 1);
-
-% label the axes
-xlabel(groupDir1);
-ylabel(groupDir2);
 
 % revert hold state
 if ~wasHold
