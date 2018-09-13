f2ae608ec518624ba7ebc1f57b021c21a7b924be
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-09-13 15:40:01 -0400

    Make comparison plots using new labeling framework

--- papers/ternary_salience/plotTernaryNIPPComparison.m
+++ papers/ternary_salience/plotTernaryNIPPComparison.m
@@ -11,6 +11,7 @@ load(fullfile('save', 'TernaryNIPredictions_PennNoSky_2x32.mat'));
 
 %% Single planes
 
+plusMinus = '+?';
 plotTernaryMatrix({predictions, ternaryAvg}, 'ellipse', false, ...
     'groupMaskFct', @(g) length(g) == 6 && ~strcmp(g(1:2), 'AC'), ...
     'beautifyOptions', {'ticks', 'off', 'ticklabels', false, ...
@@ -19,7 +20,10 @@ plotTernaryMatrix({predictions, ternaryAvg}, 'ellipse', false, ...
     'triangleOptions', {'fontscale', 0.667, 'edgelabels', 'digit'}, ...
     'limits', 1.5, ...
     'plotterOptions', {'fixedShape', [1 4], 'fixedSize', [6 1.5], ...
-    'fixedSizeUnits', 'inches'});
+    'fixedSizeUnits', 'inches'}, ...
+    'titleShift', [0 -0.5], 'titleAlignment', {'center', 'bottom'}, ...
+    'labelOptions', {'FontSize', 8, 'subscriptSpacing', -0.55, ...
+        'coeffToStr', @(i) plusMinus(i)});
 preparegraph;
 
 safePrint(fullfile('figs', 'draft', 'ternarySecondOrderNIPPMatch.pdf'));
@@ -28,31 +32,38 @@ safePrint(fullfile('figs', 'draft', 'ternarySecondOrderNIPPMatch.pdf'));
 
 fig = figure;
 fig.Units = 'inches';
-fig.Position = [2 2 6 5];
+totalX = 6;
+totalY = 5;
+fig.Position = [2 2 totalX totalY];
 
 ax = zeros(22, 1);
-% row_numbers = [3 3 5 6 5];
-% row_shifts = [3 3 0.5 0 0.5];
 rowNumbers = [2 2 6 6 6];
 rowShifts = [4 4 0 0 0];
 rowStarts = [1 1+cumsum(rowNumbers(1:end-1))];
-figX = 1/max(rowNumbers);
-figY = 1/length(rowNumbers);
+figX = totalX/max(rowNumbers);
+figY = totalY/length(rowNumbers);
+factorX = 0.75;
+factorY = 0.75;
 for i = 1:length(ax)
     crtAx = axes;
     crtRow = find(i >= rowStarts, 1, 'last');
     crtShift = rowShifts(crtRow);
     crtCol = i - rowStarts(crtRow); % 0-based!!
-    crtAx.OuterPosition = [(crtShift + crtCol)*figX 1 - crtRow*figY figX figY];
+    crtAx.Units = 'inches';
+    crtAx.OuterPosition = [(crtShift + crtCol)*figX totalY - crtRow*figY figX*factorX figY*factorY];
     ax(i) = crtAx;
 end
 
+plusMinus = '+?';
 plotTernaryMatrix({predictions, ternaryAvg}, 'ellipse', false, ...
     'groupMaskFct', @(g) sum(g == ';') == 1, ...
     'beautifyOptions', {'ticks', 'off', 'ticklabels', false, ...
         'titlesize', 12, 'titleweight', 'normal', 'noaxes', true, ...
         'fontscale', 0.667}, ...
-    'plotterOptions', {'fixedAxes', ax});
+    'plotterOptions', {'fixedAxes', ax}, ...
+    'labelOptions', {'FontSize', 8, 'subscriptSpacing', -0.55, ...
+        'coeffToStr', @(i) plusMinus(i)}, ...
+    'xLabelAlignment', {'center', 'bottom'}, 'yLabelAlignment', {'left', 'bottom'});
 preparegraph;
 
 safePrint(fullfile('figs', 'draft', 'ternaryMixedNIPPMatch.pdf'));
