d470db260de82a766bc593fa9d9d170c3172c3b6
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-09-13 15:36:40 -0400

    Add function that displays a formatted group name

--- graphing/textTexGroup.m
+++ graphing/textTexGroup.m
@@ -11,23 +11,39 @@ function h = textTexGroup(x, y, group, varargin)
 %   created.
 %
 %   Options (case insensitive to match Matlab's TEXT):
-%    'FontName':
+%    'FontName'
 %       Name of font to use.
-%    'FontSize':
+%    'FontSize'
 %       Font size for the top-level symbol.
 %    'Units':
 %       Units in which the `x` and `y` arguments are given (default is data
 %       units).
-%    'symbol':
+%    'HorizontalAlignment'
+%       Set how the text is aligned horizontally. This can be 'left',
+%       'center', or 'right'.
+%    'VerticalAlignment'
+%       Set how the text is aligned vertically. This can be 'middle',
+%       'top', or 'bottom'. Note that 'middle' refers to the *symbol only*,
+%       while 'top' and 'bottom' refer to the whole group name.
+%    'symbol'
 %       Symbol to use for the texture group. If not given, it uses '\gamma'
 %       for first-order group, '\beta' for second order, '\theta' for third
 %       order, and '\alpha' for fourth order.
-%    'displayZeros':
+%    'displayZeros'
 %       If `true`, displays even coefficients that are 0.
-%    'postfix':
+%    'postfix'
 %       Text to write after the group name and coefficients.
-%    'coeffToStr':
+%    'coeffToStr'
 %       Custom function to use to convert coefficients to strings.
+%    'subscriptScaling'
+%       Size of subscript font as a fraction of the symbol font.
+%    'subscriptSpacing'
+%       Extra spacing added between subscripts, in units of subscript
+%       extents. Set to a negative value to diminish spacing.
+%    'postfixSpacing'
+%       Spacing added before postfix, in units of subscript extents.
+%    'groupSpacing'
+%       Spacing added between groups, in units of subscript extents.
 
 % parse optional arguments
 parser = inputParser;
@@ -37,10 +53,16 @@ parser.FunctionName = mfilename;
 parser.addParameter('FontName', '', @(s) ischar(s) && isvector(s));
 parser.addParameter('FontSize', [], @(x) isscalar(x) && isnumeric(x) && x > 0);
 parser.addParameter('Units', '', @(s) ischar(s) && isvector(s));
+parser.addParameter('HorizontalAlignment', 'left', @(s) ismember(s, {'left', 'center', 'right'}));
+parser.addParameter('VerticalAlignment', 'middle', @(s) ismember(s, {'middle', 'top', 'bottom'}));
 parser.addParameter('symbol', '', @(s) ischar(s) && isvector(s));
 parser.addParameter('displayZeros', false, @(b) islogical(b) && isscalar(b));
 parser.addParameter('postfix', '', @(s) ischar(s) && isvector(s));
 parser.addParameter('coeffToStr', @int2str);
+parser.addParameter('subscriptScaling', 0.8, @(x) isnumeric(x) && isscalar(x) && x > 0);
+parser.addParameter('subscriptSpacing', -0.4, @(x) isnumeric(x) && isscalar(x));
+parser.addParameter('postfixSpacing', 0.1, @(x) isnumeric(x) && isscalar(x));
+parser.addParameter('groupSpacing', 0.2, @(x) isnumeric(x) && isscalar(x));
 
 % show defaults if requested
 if nargin == 1 && strcmp(x, 'defaults')
@@ -109,7 +131,7 @@ for idx = 1:length(groups)
     hSymbol = text(x, y, symbol, textOptions{:});
     
     % figure out size and spacing for subscripts
-    subscriptSize = hSymbol.FontSize*0.8;
+    subscriptSize = hSymbol.FontSize*params.subscriptScaling;
     
     tmp = uicontrol('style', 'text', 'string', 'X', ...
         'fontname', hSymbol.FontName, 'fontsize', subscriptSize, ...
@@ -119,7 +141,7 @@ for idx = 1:length(groups)
     tmp.Units = 'pixels';
     subscriptExtents = tmp.Extent(3:4);
     % the extents obtained like this are way too big...
-    subscriptExtents = subscriptExtents*0.6;
+    subscriptExtents = subscriptExtents*(1 + params.subscriptSpacing);
     
     % specify position of subscripts in pixels
     hSymbol.Units = 'pixels';
@@ -136,12 +158,12 @@ for idx = 1:length(groups)
         i = floor((k - 1)/2);
         j = mod(k - 1, 2);
         if allCoeffs(k) > 0 || params.displayZeros
-            h = text(symbolEdge(1) + j*subscriptExtents(1), symbolEdge(2) - i*subscriptExtents(2), ...
+            crtH = text(symbolEdge(1) + j*subscriptExtents(1), symbolEdge(2) - i*subscriptExtents(2), ...
                 params.coeffToStr(allCoeffs(k)), ...
                 'fontname', hSymbol.FontName, 'fontsize', subscriptSize, ...
                 'verticalalignment', 'top', 'Units', 'pixels');
             largestX = max(largestX, j*subscriptExtents(1));
-            hCoeffs = [hCoeffs h]; %#ok<AGROW>
+            hCoeffs = [hCoeffs crtH]; %#ok<AGROW>
         end
     end
     
@@ -153,20 +175,57 @@ for idx = 1:length(groups)
         postfix = strcat(bracket, ';');
     end
     if ~isempty(postfix)
-        bracketPosition = symbolEdge + [largestX + subscriptExtents(1)*1.1, 0];
-        h = text(bracketPosition(1), bracketPosition(2), postfix, 'fontname', ...
+        bracketPosition = symbolEdge + ...
+            [largestX + subscriptExtents(1)*(1 + params.postfixSpacing), 0];
+        crtH = text(bracketPosition(1), bracketPosition(2), postfix, 'fontname', ...
             hSymbol.FontName, 'fontsize', hSymbol.FontSize, 'Units', 'pixels');
-        hCoeffs = [hCoeffs h]; %#ok<AGROW>
+        hCoeffs = [hCoeffs crtH]; %#ok<AGROW>
     end
     
     h = [h hSymbol hCoeffs]; %#ok<AGROW>
     
     % move to next group
-    x = h(end).Extent(1) + h(end).Extent(3) + subscriptSize*0.2;
+    x = h(end).Extent(1) + h(end).Extent(3) + subscriptExtents(1)*params.groupSpacing;
     y = h(end).Position(2);
     
     % subsequent units are always pixels
     units = 'pixels';
+end
+
+if ~strcmp(params.HorizontalAlignment, 'left') || ~strcmp(params.VerticalAlignment, 'middle')
+    % find the full extents of the group name
+    allExtent = cell2mat({h.Extent}');
+    
+    % convert to [min_x, min_y, max_x, max_y]
+    actualExtents = [allExtent(:, 1:2) allExtent(:, 1:2) + allExtent(:, 3:4)];
+    
+    fullExtents = [min(actualExtents(:, 1:2), [], 1) max(actualExtents(:, 3:4), [], 1)];
+    
+    dimensions = fullExtents(3:4) - fullExtents(1:2);
+    
+    shift = [0 0];
+    switch params.HorizontalAlignment
+        case 'center'
+            shift(1) = -dimensions(1) / 2;
+        case 'right'
+            shift(1) = -dimensions(1);
+    end
+    
+    % get original y value in pixels
+    h(1).Units = 'pixels';
+    originalY = h(1).Position(2);
+    
+    switch params.VerticalAlignment
+        case 'top'
+            shift(2) = originalY - fullExtents(4);
+        case 'bottom'
+            shift(2) = originalY - fullExtents(2);
+    end
+
+    for i = 1:length(h)
+        h(i).Units = 'pixels'; %#ok<AGROW>
+        h(i).Position(1:2) = h(i).Position(1:2) + shift; %#ok<AGROW>
+    end
 end
 
 end
