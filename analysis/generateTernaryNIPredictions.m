3060faf2de313dc8b0d1bc0e114593ae351ab794
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-09-21 09:23:05 -0400

    Improve display of predictions

--- analysis/generateTernaryNIPredictions.m
+++ analysis/generateTernaryNIPredictions.m
@@ -137,14 +137,72 @@ outFileName = ['TernaryNIPredictions_' dbChoice compressExt '_' NRstr ...
 save(fullfile('save', outFileName), 'NRselection', 'restrictToFocus', ...
     'gain', 'predictions', 'predictionDetails', 'gainTransformFct');
 
-%% Check match in single planes
+%% Check match
 
-plotTernaryMatrix({predictions, pp}, ...
-    'ellipse', false, ...
-    'groupMaskFct', @(group) ~strcmp(group, 'A_1') && sum(group == ';') == 0);
+fig = figure;
+fig.Units = 'inches';
+totalX = 6;
+totalY = 5;
+fig.Position = [2 2 totalX totalY];
 
-%% Check match in mixed planes
+% axes for mixed planes
+multi_ax = zeros(22, 1);
+rowNumbers = [2 2 6 6 6];
+rowShifts = [4 4 0 0 0];
+rowStarts = [1 1+cumsum(rowNumbers(1:end-1))];
+figX = totalX/max(rowNumbers);
+figY = totalY/length(rowNumbers);
+factorX = 0.75;
+factorY = 0.75;
+for i = 1:length(multi_ax)
+    crtAx = axes;
+    
+    crtRow = find(i >= rowStarts, 1, 'last');
+    crtShift = rowShifts(crtRow);
+    crtCol = i - rowStarts(crtRow); % 0-based!!
+    
+    crtAx.Units = 'inches';
+    crtAx.OuterPosition = [(crtShift + crtCol)*figX totalY - crtRow*figY figX*factorX figY*factorY];
+    
+    multi_ax(i) = crtAx;
+end
 
-plotTernaryMatrix({predictions, pp}, ...
-    'ellipse', false, ...
-    'groupMaskFct', @(group) sum(group == ';') == 1);
+% axes for single planes
+single_ax = zeros(4, 1);
+for i = 1:length(single_ax)
+    crtAx = axes;
+    
+    crtRow = floor((i-1)/2);
+    crtCol = mod(i-1, 2);
+    
+    crtAx.Units = 'inches';
+    crtAx.OuterPosition = [crtCol*2*figX totalY - (1 + crtRow)*figY figX figY];
+    
+    single_ax(i) = crtAx;
+end
+
+% plot single planes
+plusMinus = '+?';
+plotTernaryMatrix({predictions, pp}, 'ellipse', false, ...
+    'groupMaskFct', @(g) length(g) == 6 && ~strcmp(g(1:2), 'AC'), ...
+    'beautifyOptions', {'ticks', 'off', 'ticklabels', false, ...
+        'titlesize', 12, 'titleweight', 'normal', 'noaxes', true, ...
+        'fontscale', 0.667}, ...
+    'triangleOptions', {'fontscale', 0.667, 'edgelabels', 'digit'}, ...
+    'limits', 1.5, ...
+    'plotterOptions', {'fixedAxes', single_ax}, ...
+    'titleShift', [0 -0.5], 'titleAlignment', {'center', 'bottom'}, ...
+    'labelOptions', {'FontSize', 8, 'subscriptSpacing', -0.55, ...
+        'coeffToStr', @(i) plusMinus(i)});
+
+% plot mixed planes
+plotTernaryMatrix({predictions, pp}, 'ellipse', false, ...
+    'groupMaskFct', @(g) sum(g == ';') == 1, ...
+    'beautifyOptions', {'ticks', 'off', 'ticklabels', false, ...
+        'titlesize', 12, 'titleweight', 'normal', 'noaxes', true, ...
+        'fontscale', 0.667}, ...
+    'plotterOptions', {'fixedAxes', multi_ax}, ...
+    'labelOptions', {'FontSize', 8, 'subscriptSpacing', -0.55, ...
+        'coeffToStr', @(i) plusMinus(i)}, ...
+    'xLabelAlignment', {'center', 'bottom'}, 'yLabelAlignment', {'left', 'bottom'});
+preparegraph;
